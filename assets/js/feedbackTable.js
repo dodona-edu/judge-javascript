fa253e924d3d039adb64f9f8b3b4d048864850b7
dmouriss <dieter.mourisse@ugent.be>
2017-04-21 17:11:19 +0200

    updated version

--- assets/js/feedbackTable.js
+++ assets/js/feedbackTable.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var worker = require('./workers/judge_worker.js')
+var parallel = require('dimas-parallel');
 
 function FeedbackTable(tests) {
     this.correct = 0;
@@ -7,107 +8,8 @@ function FeedbackTable(tests) {
     this.tests = tests;
 }
 
-FeedbackTable.method = function(name, body) {
-    FeedbackTable.prototype[name] = body;
-}
-
-// add row to feedback table
-FeedbackTable.method('add', function (args) {
-    console.log('add');
-    return;
-
-    // define row templates
-    var row_base_template = '<tr class="feedback-row {type} {eval} {color}">{cols}</tr>';
-    var row_span_template = row_base_template.format({
-        "cols": '<td colspan="2">{content}</td>'
-    });
-    var row_icon_template = row_base_template.format({
-        "cols": '<td class="icon"><span class="glyphicon glyphicon-{icon}" style="color:{icon-color};"></span></td><td>{content}</td>'
-    });
-    var row_statement = row_span_template.format({
-        "type": "statement",
-        "content": '&#x25BA; {content}'
-    });
-    var row_error = row_icon_template.format({
-        "type": "error",
-        "color": "danger",
-        "icon": "exclamation-sign",
-        "icon-color": "red"
-    });
-    var row_correct = row_icon_template.format({
-        "type": "output",
-        "icon": "ok",
-        "icon-color": "green"
-    });
-    var row_wrong = row_icon_template.format({
-        "type": "output",
-        "icon": "remove",
-        "icon-color": "red"
-    });
-
-    // define row types
-    var row = {
-        "statement": row_statement,
-        "error": row_error,
-        "correct": row_correct,
-        "wrong": row_wrong
-    };
-
-    // define default colors types
-    var color = {
-        "statement": "active",
-        "error": "danger",
-        "AC": "success",
-        "WA": "danger",
-        "IE": "danger",
-        "RE": "danger"
-    };
-
-    $("#feedback-table").append(row[args.type || "error"].format({
-        "eval": args.eval || "IE",
-        "color": args.color || color[args.type] || color[args.eval] || "danger",
-        "content": args.content || "JudgeError: unknown error"
-    }));
-
-});
-
-// show error message in feedback table
-FeedbackTable.method('error', function (error) {
-    console.log('error')
-    return
-    var feedbackTable = this;
-
-    // show error message
-    feedbackTable.add({
-        "type": "error",
-        "eval": "IE",
-        "content": displayError(error)
-    });
-
-    // hide loading icon of feedback table
-    $("#feedback-loading").hide();
-});
-
-
-// clear feedback table
-FeedbackTable.method('clear', function () {
-    console.log('clear');
-    return
-    // empty feedback table
-    $("#feedback-table").html('');
-
-    // initialize badges
-    this.correct = 0;
-    this.wrong = 0;
-    $("#tests-correct").html(this.correct);
-    $("#tests-wrong").html(this.wrong);
-
-    // initialize feedback menu
-    $("#feedback-menu-toggle-correct a").html('verberg correct');
-});
-
 // test submitted source code
-FeedbackTable.method('test', function (args) {
+FeedbackTable.prototype.test = function (args) {
     var feedbackTable = this;
     var source = args.source || '';
     var currentStatement;
@@ -133,73 +35,19 @@ FeedbackTable.method('test', function (args) {
         }, ms);
 
     }
-
-    // clear the feedback table
-    feedbackTable.clear();
-
     var judge = worker.Judge(code);
-
-    // perform actions if test case has been evaluated
-    judge.onmessage = function (e) {
-        // adjust judge based on incoming setting
-        if (e.data.type === "setting") {
-            if (e.data.name === "timeout") {
-                // set new timeout
-                setCustomTimeout(e.data.value);
-            }
-
-            // stop further processing
-            return;
-        }
-
-        // translate messages from judge into feedback table rows
-        e.data.rows.forEach(function (row) {
-            feedbackTable.add(row);
-        });
-
-        // increment badge corresponding to outcome of evaluation
-        if (e.data.eval == "AC") {
-            feedbackTable.correct += (e.data.weight || 1);
-            $("#tests-correct").html(feedbackTable.correct);
-        } else {
-            feedbackTable.wrong += (e.data.weight || 1);
-            $("#tests-wrong").html(feedbackTable.wrong);
-        }
-
-        // check if additional results are expected
-        if (e.data.done) {
-            // hide loading icon
-            $("#feedback-loading").hide();
-
-            // terminate the worker
-            judge.terminate();
-
-            // clear the timer
-            clearTimeout(timer);
-            resolve({correct: feedbackTable.correct, wrong: feedbackTable.wrong, status: "done"});
-        }
-    };
-
-    judge.onerror = function (e) {
-        // show error message from Web Worker
-        feedbackTable.error(e);
-    };
 
     // set default timeout
     setCustomTimeout();
     for (var i=0; i < feedbackTable.tests.length; i++) {
         eval(feedbackTable.tests[i]);
     }
-    judge.run(code, tests);
 
+    result = judge.run(code, tests);
+    console.log(result);
+    clearTimeout(timer);
     return;
-
-    // execute source code and tests cases
-    judge.postMessage({
-        "source": source,
-        "tests": feedbackTable.tests
-    });
-});
+};
 
 var tests = fs.readFileSync('tests.js').toString().split('\n');
 var code = fs.readFileSync('code.js').toString();
--- assets/js/workers/judge_worker.js
+++ assets/js/workers/judge_worker.js
@@ -1,15 +1,8 @@
 module.exports = {
 
-    // start evaluation when source code is received
-    onmessage: function (e) {
-        // create new JavaScript Judge for submitted source code
-        judge = new Judge(e.data.source, e.data.tests);
-
-        // execute source code and tests
-        judge.run();
-    },
-
     Judge: function(sourceCode) {
+        var deepEqual = require('deep-equal')
+
         // private variables
         var test_cases = [];
         var test_index = 0;
@@ -39,17 +32,13 @@ module.exports = {
 
         // set custom timeout
         function setCustomTimeout(ms) {
-
-            postMessage({
-                "type": "setting",
-                "name": "timeout",
-                "value": ms
-            });
+            console.log('Time Limit Exceeded')
 
         }
 
         // execute test cases in queued order
         function run() {
+            var context = {'status': 'CA', 'testcases': []};
             // start timer that will be fired upon timeout
             // startTimer();
             try {
@@ -58,25 +47,17 @@ module.exports = {
             } catch (e) {
                 console.log('runtime error');
                 return;
-                // add runtime error to feedback table
-                postMessage({
-                    "rows": [{
-                        "type": "error",
-                        "eval": "CE",
-                        "content": displayError(e, true)
-                    }],
-                    "eval": "CE",
-                    "weight": 1,
-                    "done": true
-                });
-
-                // stop any further processing of test cases
-                return;
             }
+            context.status = 'CA'
             while (test_index < test_cases.length) {
                 // execute the next available test case
-                exec_test.apply(undefined, test_cases[test_index++]);
+                testcase = exec_test.apply(undefined, test_cases[test_index++]);
+                if (testcase.status !== 'CA') {
+                    context.status = testcase.status;
+                }
+                context.testcases.push(testcase);
             }
+            return context;
         }
 
         function exec_test(expression, expected, comparison) {
@@ -84,12 +65,7 @@ module.exports = {
             currentExpression = expression;
 
             // prepare object that will be returned to main thread
-            var feedback = {
-                "rows": [],
-                "eval": "IE",
-                "weight": 1,
-                "done": test_index == test_cases.length
-            }
+            var testcase = {};
 
             //
             // check correctness of submitted solution for this test case
@@ -100,6 +76,7 @@ module.exports = {
             // check if exception was expected and adjust expected if this is the
             // case
             var expected_exception = false;
+            str_expected = display(expected);
             try {
                 // check if expected exception is a string that starts with
                 // "expection:" prefix
@@ -114,10 +91,10 @@ module.exports = {
             // evaluate expression and set generated result accordingly
             var generated_exception = false;
             try {
-                generated = evaluate(expression);
-
+                generated = evaluate(expression);            
+                str_generated = display(generated);
                 if (comparison === undefined) {
-                    comparison = equals;
+                    comparison = deepEqual;
                 }
 
                 // check whether generated result is as expected
@@ -132,311 +109,126 @@ module.exports = {
 
                 // convert exception into string
                 generated = e;
-                //str_generated = displayError(e);
-
+                str_generated = displayError(e);
                 // check whether generated exception is as expected
                 correct = (
                     expected_exception &&
-                    equals(str_expected, str_generated)
+                    deepEqual(str_expected, str_generated)
                 );
             }
-
             // set evaluation status of feedback
-            feedback.eval = correct ? "AC" : "WA";
-
-            //
-            // compile feedback that must be added to feedback table
-            //
-
-            // add statement/expression tested to feedback table
-            feedback.rows.push({
-                "type": "statement",
-                "content": expression
-            });
-
             if (expected !== undefined || generated !== undefined) {
-                console.log(expected);
-                console.log(generated);
-                console.log("==========");
-            }
-
-            return;
-
-            // add expected and generated output to feedback table
-            if (expected !== undefined || generated !== undefined) {
-                // convert expected result to string
-                if (!expected_exception) {
-                }
-
-                // convert generated result to string
-                if (!generated_exception) {
-                }
-
-                if (!correct && generated_exception && !expected_exception) {
-                    feedback.eval = "RE";
-                    feedback.rows.push({
-                        "type": "error",
-                        "eval": feedback.eval,
-                        "content": str_generated
-                    });
-                } else if (!correct) {
-                    var diff_expected = '',
-                        diff_generated = '';
-
-                    // determine whether results should be diffed
-                    var show_diffed = true;
-                    if (
-                        expected_exception !== generated_exception &&
-                        (expected_exception || generated_exception)
-                    ) {
-                        // no diff if only one exception thrown
-                        show_diffed = false;
-                    } else if (
-                        expected === undefined || expected === null ||
-                        generated === undefined || generated === null
-                    ) {
-                        // no diff if either of the results is undefined or null
-                        show_diffed = false;
-                    }
-
-                    // diff results if needed
-                    if (show_diffed) {
-                        // diff string representations of expected and generated
-                        // results
-                        var diff = JsDiff.diffChars(str_expected, str_generated);
-
-                        // compose diffed representations of expected and generated
-                        // results
-                        diff.forEach(function (part) {
-                            if (part.added) {
-                                diff_generated += (
-                                    '<span class="removed">{part}</span>'.format({
-                                        "part": part.value
-                                    })
-                                );
-                            } else if (part.removed) {
-                                diff_expected += (
-                                    '<span class="added">{part}</span>'.format({
-                                        "part": part.value
-                                    })
-                                );
-                            } else {
-                                diff_expected += part.value;
-                                diff_generated += part.value;
-                            }
-                        });
-                    }
-
-                    feedback.rows.push({
-                        "type": "correct",
-                        "content": show_diffed ? diff_expected : str_expected
-                    });
-
-                    feedback.rows.push({
-                        "type": "wrong",
-                        "content": show_diffed ? diff_generated : str_generated
-                    });
-
+                if (generated_exception && !expected_exception) {
+                    testcase.status = "RE";
+                    testcase.message = str_generated;
                 } else {
-
-                    feedback.rows.push({
-                        "type": "correct",
-                        "content": str_generated
-                    });
-
+                    testcase.generated = str_generated;
+                    testcase.expected = str_expected;
+                    if (!correct) { 
+                        testcase.status = "WA"
+                    } else {
+                        testcase.status = 'CA'
+                    }
                 }
-
             }
 
-            return;
-            // set evaluation status of rows to global evaluation status
-            feedback.rows.forEach(function (row) {
-                row.eval = feedback.eval;
-            });
-
-            // return feedback to the main thread
-            postMessage(feedback);
-
-            // clear the current expression
-            currentExpression = undefined;
+            return testcase;
         };
+    
 
-        // helper function for testing the equality of two given values
-        function equals(obj1, obj2) {
-            var i, key;
+        function display(obj) {
+            var str = '',
+                keys = [],
+                key, i;
 
-            if (Array.isArray(obj1)) {
-                // check if second object is also an array
-                if (!Array.isArray(obj2)) {
-                    return false;
-                }
-
-                // check if both arrays have the same length
-                if (obj1.length !== obj2.length) {
-                    return false;
-                }
-
-                // check if corresponding objects are the same
-                for (i = 0; i < obj1.length; ++i) {
-                    if (!equals(obj1[i], obj2[i])) {
-                        return false;
+            if (obj === undefined) {
+                return 'undefined';
+            } else if (obj === null) {
+                return 'null';
+            } else if (Array.isArray(obj)) {
+                for (i = 0; i < obj.length; ++i) {
+                    if (str) {
+                        str += ', ';
                     }
+                    str += display(obj[i]);
                 }
-
-                // both arrays are the same
-                return true;
-            } else if (typeof obj1 === undefined) {
-                return obj2 === undefined;
-            } else if (obj1 === null) {
-                return obj2 === null;
-            } else if (typeof obj1 === 'object') {
-                // check if second object is also an object
-                if (typeof obj2 !== 'object') {
-                    return false;
-                }
-                if (obj1.hasOwnProperty !== undefined &&
-                    obj2.hasOwnProperty !== undefined &&
-                    typeof obj1.hasOwnProperty === 'function' &&
-                    typeof obj2.hasOwnProperty === 'function') {
-
-                    // check if all keys of obj1 are in obj2 with the same value
-                    for (key in obj1) {
-                        if (obj1.hasOwnProperty(key) && !equals(obj1[key], obj2[key])) {
-                            return false;
+                return '[' + str + ']';
+            } else if (typeof obj === 'object') {
+                // pretty print object
+                if (obj.hasOwnProperty !== undefined &&
+                    typeof obj.hasOwnProperty === 'function') {
+                    for (key in obj) {
+                        if (obj.hasOwnProperty(key)) {
+                            keys.push(key);
                         }
                     }
-
-                    // check if all keys of obj1 are in obj2 with the same value
-                    for (key in obj2) {
-                        if (obj2.hasOwnProperty(key) && !equals(obj1[key], obj2[key])) {
-                            return false;
-                        }
-                    }
-
-                    // both objects are the same
-                    return true;
                 } else {
-                    // check if all keys of obj1 are in obj2 with the same value
-                    for (key in obj1) {
-                        if (!equals(obj1[key], obj2[key])) {
-                            return false;
-                        }
-                    }
-
-                    // check if all keys of obj1 are in obj2 with the same value
-                    for (key in obj2) {
-                        if (!equals(obj1[key], obj2[key])) {
-                            return false;
-                        }
-                    }
-
-                    // both objects are the same
-                    return true;
-                }
-            } else {
-                // check if both objects are the same using the === operator
-                return obj1 === obj2;
-            }
-        }
-    },
-
-    display: function(obj) {
-        var str = '',
-            keys = [],
-            key, i;
-
-        if (obj === undefined) {
-            return 'undefined';
-        } else if (obj === null) {
-            return 'null';
-        } else if (Array.isArray(obj)) {
-            for (i = 0; i < obj.length; ++i) {
-                if (str) {
-                    str += ', ';
-                }
-                str += display(obj[i]);
-            }
-            return '[' + str + ']';
-        } else if (typeof obj === 'object') {
-            // pretty print object
-            if (obj.hasOwnProperty !== undefined &&
-                typeof obj.hasOwnProperty === 'function') {
-                for (key in obj) {
-                    if (obj.hasOwnProperty(key)) {
+                    for (key in obj) {
                         keys.push(key);
                     }
                 }
+                keys.sort();
+                for (i = 0; i < keys.length; ++i) {
+                    if (str) {
+                        str += ', ';
+                    }
+                    str += display(keys[i]) + ': ' + display(obj[keys[i]]);
+                }
+                return '{' + str + '}';
             } else {
-                for (key in obj) {
-                    keys.push(key);
+                if (typeof obj === 'string') {
+                    // return "'" + obj + "'";
+                    var repr = JSON.stringify(obj);
+                    if (
+                        repr.indexOf("'") === -1 &&
+                        repr.slice(1, -1).indexOf('\\"') >= 0
+                    ) {
+                        repr = "'{repr}'".format({
+                            repr: repr.slice(1, -1).replace('\\"', '"', "g")
+                        });
+                    }
+                    return repr;
+                } else {
+                    // pretty print general object
+                    return obj.toString();
                 }
-            }
-            keys.sort();
-            for (i = 0; i < keys.length; ++i) {
-                if (str) {
-                    str += ', ';
-                }
-                str += display(keys[i]) + ': ' + display(obj[keys[i]]);
-            }
-            return '{' + str + '}';
-        } else {
-            if (typeof obj === 'string') {
-                // return "'" + obj + "'";
-                var repr = JSON.stringify(obj);
-                if (
-                    repr.indexOf("'") === -1 &&
-                    repr.slice(1, -1).indexOf('\\"') >= 0
-                ) {
-                    repr = "'{repr}'".format({
-                        repr: repr.slice(1, -1).replace('\\"', '"', "g")
-                    });
-                }
-                return repr;
-            } else {
-                // pretty print general object
-                return obj.toString();
             }
         }
-    },
 
-    // helper function for converting Error objects to string
-    displayError: function(e, showLine) {
-        try {
-            if (typeof e === "string") {
-                return e;
-            } else {
-                // format message
-                if (e.name !== undefined && e.message !== undefined) {
-                    // add line number if available
-                    if (
-                        // check if line number is available
-                        e.lineNumber &&
-                        // check if stack trace is available
-                        e.stack &&
-                        // check if stack trace goes deeper than error in statement
-                        // that is being executed (in other words: in the submitted
-                        // source code)
-                        e.stack.split("\n").length != 5
-                    ) {
-                        message = "{name} (line {line}): {message}";
-                    } else {
-                        message = "{name}: {message}";
-                    }
-                    message = message.format({
-                        name: e.name,
-                        message: e.message,
-                        line: e.lineNumber
-                    });
+        // helper function for converting Error objects to string
+        function displayError(e, showLine) {
+            try {
+                if (typeof e === "string") {
+                    return e;
                 } else {
-                    message = "JudgeError: ill-formed Error";
-                    if (display(e) !== "") {
-                        message += ": " + display(e)
+                    // format message
+                    if (e.name !== undefined && e.message !== undefined) {
+                        // add line number if available
+                        if (
+                            // check if line number is available
+                            e.lineNumber &&
+                            // check if stack trace is available
+                            e.stack &&
+                            // check if stack trace goes deeper than error in statement
+                            // that is being executed (in other words: in the submitted
+                            // source code)
+                            e.stack.split("\n").length != 5
+                        ) {
+                            message = e.name + " (line " + e.lineNumber + "): " + e.message;
+                        } else {
+                            message = e.name + ": " + e.message;
+                        }
+                    } else {
+                        message = "JudgeError: ill-formed Error";
+                        if (display(e) !== "") {
+                            message += ": " + display(e)
+                        }
                     }
+                    return message;
                 }
-                return message;
+            } catch (e) {
+                return e.toString();
             }
-        } catch (e) {
-            return e.toString();
         }
     }
 }
