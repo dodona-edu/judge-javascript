218fca6986f36dc07a683f2cf2ce1d9f3b20097a
pdawyndt <peter.dawyndt@ugent.be>
2017-04-25 13:29:07 +0200

    js-judge:replace tabs by spaces

--- assets/js/judge.js
+++ assets/js/judge.js
@@ -4,136 +4,136 @@ var dodona = require('./dodona.js');
 // var parallel = require('dimas-parallel');
 
 var Judge = function(testfile) {
-	
-	// data structure that contains all test results
-	this.submission = new dodona.Submission();
-	
-	// parse test file
-	this.parseTests(testfile);
-	
+    
+    // data structure that contains all test results
+    this.submission = new dodona.Submission();
+    
+    // parse test file
+    this.parseTests(testfile);
+    
 };
 
 // parse test cases
 Judge.prototype.parseTests = function(testfile) {
-	
-	var self = this;
-	
-	// create parser for test cases
-	var TestParser = function() {};
-	TestParser.prototype.test = function(expression, expected, comparison) {
-		
-		// for now, each testcase is in its own context
-		self.submission.addContext(new dodona.Context());
-		
-		// add expression as testcase
-		self.submission.addTestCase(
-			new dodona.TestCase({
-				description: expression
-			})
-		);
-		
-		// determine channel of expected result
-		var channel = 'return';
-		if (typeof expected === 'string' && expected.slice(0, 10) === 'exception:') {
-			channel = 'exception';
+    
+    var self = this;
+    
+    // create parser for test cases
+    var TestParser = function() {};
+    TestParser.prototype.test = function(expression, expected, comparison) {
+        
+        // for now, each testcase is in its own context
+        self.submission.addContext(new dodona.Context());
+        
+        // add expression as testcase
+        self.submission.addTestCase(
+            new dodona.TestCase({
+                description: expression
+            })
+        );
+        
+        // determine channel of expected result
+        var channel = 'return';
+        if (typeof expected === 'string' && expected.slice(0, 10) === 'exception:') {
+            channel = 'exception';
             // remove "expection:" prefix in case of expected exception
-			expected = expected.slice(10);
-		}
-		
-		// add expected result
-		self.submission.addTest(
-			new dodona.Test({
-				expected: expected,
-				data: { 
-					channel: channel,
-					evaluation: {
-						comparison: comparison
-					}
-				}
-			})
-		);
-		
-	};
-	
-	// create judge object used in description of test cases
-	var judge = new TestParser();
-	
-	// evaluate all tests
-	var tests = fs.readFileSync(testfile).toString().split('\n');
+            expected = expected.slice(10);
+        }
+        
+        // add expected result
+        self.submission.addTest(
+            new dodona.Test({
+                expected: expected,
+                data: { 
+                    channel: channel,
+                    evaluation: {
+                        comparison: comparison
+                    }
+                }
+            })
+        );
+        
+    };
+    
+    // create judge object used in description of test cases
+    var judge = new TestParser();
+    
+    // evaluate all tests
+    var tests = fs.readFileSync(testfile).toString().split('\n');
     for (var statement of tests) {
         eval(statement);
     }
-	
+    
 }
 
 Judge.prototype.run = function(sourcefile) {
-	
-	// read source file
-	var code = fs.readFileSync(sourcefile).toString();
-	
-	// evaluate each context
-	for (var tab of this.submission) {
-		for (var context of tab) {
-			this.evaluate(code, context);
-		}
-	}	
-	
-	// return feedback as JSON string
-	return this.submission.toString();
-	
+    
+    // read source file
+    var code = fs.readFileSync(sourcefile).toString();
+    
+    // evaluate each context
+    for (var tab of this.submission) {
+        for (var context of tab) {
+            this.evaluate(code, context);
+        }
+    }    
+    
+    // return feedback as JSON string
+    return this.submission.toString();
+    
 }
 
 Judge.prototype.evaluate = function(code, context) {
-	
+    
     // execute source code in the global scope
-	// TODO: this should be executed in a separate scope
-	// TODO: catch compilation errors
-	eval(code);
-	
-	// check equality of JavaScript objects
+    // TODO: this should be executed in a separate scope
+    // TODO: catch compilation errors
+    eval(code);
+    
+    // check equality of JavaScript objects
     var deepEqual = require('deep-equal');
 
-	for (var testcase of context) {
-		
-		// extract information from testcase
-		var expression = testcase.getProperty('description');
-		var tests = {};
-		for (test of testcase) {
-			tests[test.getProperty('data')['channel']] = test;
-		}
-		var comparison = test.getProperty('data')['evaluation']['comparison'];
+    for (var testcase of context) {
+        
+        // extract information from testcase
+        var expression = testcase.getProperty('description');
+        var tests = {};
+        for (test of testcase) {
+            tests[test.getProperty('data')['channel']] = test;
+        }
+        var comparison = test.getProperty('data')['evaluation']['comparison'];
         if (comparison === undefined) {
             comparison = deepEqual;
         }
 
-		// evaluate expression
-		var generated, 
-			expected,
-			args,
-			correct;
-		
+        // evaluate expression
+        var generated, 
+            expected,
+            args,
+            correct;
+        
         try {
             
-        	// evaluate expression
-        	generated = eval(expression);
+            // evaluate expression
+            generated = eval(expression);
             
             // check whether generated result is as expected
             if ('exception' in tests) {
-            	
-            	// unexpected return value
-            	testcase.addTest(new dodona.Test({
-    				generated: utils.display(generated),
-    				data: { channel: 'return' }
-        		}));
-            	
-            	tests['exception'].update({
-            		status: 'wrong answer'
-            	});
-            	
-            	
+                
+                // unexpected return value
+                testcase.addTest(new dodona.Test({
+                    generated: utils.display(generated),
+                    data: { channel: 'return' }
+                }));
+                
+                tests['exception'].update({
+                    status: 'wrong answer'
+                });
+                
+                
             } else {
-            	
-            	// compare expected and generated return values
+                
+                // compare expected and generated return values
                 expected = tests['return'].getProperty('expected');
                 args = [expected, generated]
                 args = args.concat([].slice.call(arguments, 3));
@@ -141,49 +141,49 @@ Judge.prototype.evaluate = function(code, context) {
                 
                 // update return channel
                 tests['return'].update({
-                	status: correct ? 'correct answer' : 'wrong answer',
-                	expected: utils.display(expected),
-                	generated: utils.display(generated)
+                    status: correct ? 'correct answer' : 'wrong answer',
+                    expected: utils.display(expected),
+                    generated: utils.display(generated)
                 });
-            	
+                
             }
             
         } catch (e) {
-        	
+            
             generated = utils.displayError(e);
 
             // check whether exception is as expected
             if ('exception' in tests) {
-        	
-	            // compare expected and generated exceptions
-	            expected = tests['exception'].getProperty('expected');
-	            correct = deepEqual(expected, generated);
-	            
+            
+                // compare expected and generated exceptions
+                expected = tests['exception'].getProperty('expected');
+                correct = deepEqual(expected, generated);
+                
                 // update exception channel
                 tests['exception'].update({
-                	status: correct ? 'correct answer' : 'wrong answer',
-                	generated: generated
+                    status: correct ? 'correct answer' : 'wrong answer',
+                    generated: generated
                 });
-            	
+                
             } else {
-            	
-            	// unexpected exception
-            	testcase.addTest(new dodona.Test({
-    				generated: generated,
-    				data: { channel: 'exception' }
-        		}));
-            	
-            	tests['return'].update({
-            		status: 'runtime error'
-            	});
-            	
+                
+                // unexpected exception
+                testcase.addTest(new dodona.Test({
+                    generated: generated,
+                    data: { channel: 'exception' }
+                }));
+                
+                tests['return'].update({
+                    status: 'runtime error'
+                });
+                
             }
             
         }
         
-	}
-	return;
-    	
+    }
+    return;
+        
 
     // private variables
     var test_cases = [];
@@ -321,5 +321,5 @@ Judge.prototype.evaluate = function(code, context) {
 }
 
 module.exports = {
-	Judge: Judge
+    Judge: Judge
 };
