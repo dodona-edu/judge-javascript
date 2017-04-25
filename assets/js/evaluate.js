05a11153d6e6d9f7efaad03f51eed7a8bf801953
pdawyndt <peter.dawyndt@ugent.be>
2017-04-25 14:07:28 +0200

    js-judge:update tests

--- assets/js/evaluate.js
+++ assets/js/evaluate.js
@@ -5,11 +5,7 @@ config = JSON.parse((fs.readFileSync('/dev/stdin').toString()));
 var jsJudge = require('./judge.js');
 
 // process tests
-<<<<<<< HEAD
-var js = new jsJudge.Judge('tests.js');
-=======
 var js = new jsJudge.Judge(config['resources'] + 'tests.js');
->>>>>>> branch 'dodona-version' of git@github.ugent.be:dodona/judge-javascript.git
 
 // evaluate tests
 console.log(js.run(config['source']));
