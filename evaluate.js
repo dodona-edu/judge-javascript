a9b3e576be7711e230f6655a447131d57759cb01
pdawyndt <peter.dawyndt@ugent.be>
2017-04-27 19:26:25 +0200

    js:judge:cleanup evaluate

--- evaluate.js
+++ evaluate.js
@@ -1,8 +1,9 @@
-var fs = require('fs');
-var path = require('path')
-
 process.stdin.on('data', function(data) {
 	
+	var fs = require('fs');
+	var path = require('path')
+    var judge = require('./judge.js');
+
 	// parse JSON with configuration settings from stdin 
     var config = JSON.parse(data);
     
@@ -10,11 +11,8 @@ process.stdin.on('data', function(data) {
     var resourcesDir = config['resources'];
     var sourceFile = config['source'];
     
-    // import js-judge module
-    var jsJudge = require('./judge.js');
-
     // process tests
-    var js = new jsJudge.Judge(path.join(resourcesDir, 'tests.js'));
+    var js = new judge.Judge(path.join(resourcesDir, 'tests.js'));
 
     // evaluate tests and output result to stdout
     console.log(js.run(sourceFile));     
