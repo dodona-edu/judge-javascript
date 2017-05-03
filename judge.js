0561c83a9f985dd01cca62db044278948c339b68
pdawyndt <peter.dawyndt@ugent.be>
2017-05-03 16:57:01 +0200

    js:judge:add extra newlines

--- judge.js
+++ judge.js
@@ -432,7 +432,6 @@ Judge.prototype.evaluateTestcase = function(testcase, options, sandbox) {
 	        			);
             		
             		// NOTE: add spurious newline due to bug in diff
-            		expected["return"].expected += "\n";
             		expected["return"].generated += "\n";
             		
             		
@@ -453,7 +452,6 @@ Judge.prototype.evaluateTestcase = function(testcase, options, sandbox) {
 
             		// NOTE: add extra newline due to bug in diff
             		expected["return"].expected += "\n";
-            		expected["return"].generated += "\n";
             		
             	}
             }
